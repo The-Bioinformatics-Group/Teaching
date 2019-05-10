6663a341a021ae0d94ae32829d29091b553e4a1d
mtop <mats@topel.se>
2019-05-10 13:04:48 +0200

    Moved the input files to the /db directory on all comput nodes. This will distribute the disk I/O on the system which will be needed if many students run the analysis at the same time

--- assembly/runMegahit.sh
+++ assembly/runMegahit.sh
@@ -13,8 +13,8 @@ KMER=99
 SAMPLE="test_99"
 
 # Input files in fastq format.
-FILE1=KordiaSMS9_MPL_1.fastq
-FILE2=KordiaSMS9_MPL_2.fastq
+FILE1=/db/Teaching/Assembly/Kordia/MPL/KordiaSMS9_MPL_1.fastq
+FILE2=/db/Teaching/Assembly/Kordia/MPL/KordiaSMS9_MPL_2.fastq
 
 # Run the assembly analysis
 megahit --k-list $KMER -1 $FILE1 -2 $FILE2 -t $NSLOTS -o ${SAMPLE} --out-prefix ${SAMPLE} --keep-tmp-files
